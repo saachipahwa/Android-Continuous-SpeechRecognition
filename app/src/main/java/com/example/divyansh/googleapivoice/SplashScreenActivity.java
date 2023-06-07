28dd2ab121e357e2d24007cac717da34a197c799
saachipahwa <55795994+saachipahwa@users.noreply.github.com>
2023-06-07 14:36:33 +0100

    splash screen! (white is not matching)

--- app/src/main/java/com/example/divyansh/googleapivoice/SplashScreenActivity.java
+++ app/src/main/java/com/example/divyansh/googleapivoice/SplashScreenActivity.java
@@ -9,7 +9,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
         finish();
     }
 }
